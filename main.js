097b138bd9d01ed7405ab5028eef24bd997eb200
ajshiff <ajshiffler@gmail.com>
2018-07-12 19:23:16 -0600

    in main.js removeBOM(), parseCSV(), and csvReducer have been completed. editCSV() has been started but needs some work. parent.js has been updated for testing with full constructors

--- main.js
+++ main.js
@@ -9,18 +9,21 @@ const dsv = require('d3-dsv');
  * '\ufeff' in utf8, and shows up as 'ef bb bf' in buffer form
 *********************************************************************/
 const removeBOM = function(stringWithBOM){
-    // Insert Code to remove BOM
-    console.log('Here is the removeBOM Function');
-    return 'csv without bom';
+    console.log('removeBOM()');
+    try {
+        return stringWithBOM.replace(/\ufeff/g,'');
+    } catch (error) {
+        console.error('OUTPUT FAILED. THE INPUT NEEDS TO BE A STRING:');
+        console.error(error);
+    }
 }
 
 /********************************************************************
  * This will use d3-dsv to turn the csv into a json object.
 *********************************************************************/
 const parseCSV = function(csvToParse){
-    //Parse csv into json for further use
-    console.log('Here is the parseCSV Function');
-    return ['parsed csv'];
+    console.log('parseCSV()');
+    return dsv.csvParse(csvToParse);
 }
 
 /********************************************************************
@@ -29,17 +32,28 @@ const parseCSV = function(csvToParse){
 *********************************************************************/
 const csvReducer = function(csvToReduce, options, reducerFunction){
     // Choose what to do here based on options
-    console.log('Here is the csvReducer Function');
-    csvToReduce.reduce(reducerFunction, options.initAcc);
-    return ['reduced csv'];
+    console.log('csvReducer()');
+    return csvToReduce.reduce(reducerFunction, options.initAcc);
 }
 
 /********************************************************************
- * editCSV 
+ * editCSV is kind of messy right now, but it's function may be better
+ * seperated into multiple methods
 *********************************************************************/
-const editCSV = function(reducedValue, initialCSV, options, reducerFunction){
+const editCSV = function(reducedValue, options){
     // Choose what to do here based on options
-    console.log('Here is the editCSV Function');
+    console.log('editCSV()');
+    let editedCSV;
+    reducedValue.acc(value => {
+        Object.keys(value).forEach(key => {
+            options.headersOut.forEach(header => {
+                if (key === header) {
+                    // return something here
+                }
+            });
+        });
+        // there was no match, so return the unedited acc here.
+    });
     return '\'Editted CSV Here\'';
 }
 
@@ -48,18 +62,19 @@ const editCSV = function(reducedValue, initialCSV, options, reducerFunction){
 *********************************************************************/
 class ModifiedCSV {
     constructor(initialCSV, options, reducerFunction){
-        if(initialCSV){
+        if(initialCSV !== undefined){
             this.newCSV = this.removeBOM(initialCSV);
             this.parsedCSV = this.parseCSV(this.newCSV);
             this.reducedValue = this.csvReducer(this.parsedCSV, options, reducerFunction);
-            this.newCSV = this.editCSV(this.reducedValue, this.newCSV, options, reducerFunction);
+            this.newCSV = this.editCSV(this.reducedValue, options);
         }
     }
     // methods
     removeBOM(stringWithBOM){return removeBOM(stringWithBOM);}
     parseCSV(csvToParse){return parseCSV(csvToParse);}
     csvReducer(csvToReduce, options, reducerFunction){return csvReducer(csvToReduce, options, reducerFunction);}
-    editCSV(reducedValue, initialCSV, options, reducerFunction){return editCSV(reducedValue, initialCSV, options, reducerFunction);} 
+    editCSV(reducedValue, options, reducerFunction){return editCSV(reducedValue, options);} 
+    // consider a limitKeys(parsedCSV){} method to help with editCSV
     // getters
     getObject(){return this.reducedValue;}
     getCSV(){return this.newCSV;}
--- parent.js
+++ parent.js
@@ -7,33 +7,42 @@ const csvr = require('./main.js');
 /********************************************************************
  * START: Testing with Real CSV. :START
 *********************************************************************/
-// var csv = fs.readFileSync(target, ''); // csv to test with
-// var csv = fs.readFileSync(bomRemoval, ''); // bom removal to test with
-// console.log(csv); // log to hand-check for BOM
-// csvTool = csvr();
-// let noBOMDoc = csvTool.removeBOM(csv);
-// console.log(noBOMDoc);
+var csv = fs.readFileSync(target, 'utf8');
+var options = {
+    verifyHeaders:['id', 'Country', 'gov', 'Religions'],
+    headersOut:['id', 'Country', 'gov', 'Religions'],
+    initAcc: [],
+};
+var reducer = function (acc, curr) {
+    Object.keys(curr).forEach(key => {
+        curr[key] = curr[key].replace(/[^0-9]+/g, '');
+    });
+    acc.push(curr);
+    return acc;
+}
+csvConverted = csvr(csv, options, reducer);
+// console.log(csvConverted.getObject());
 /********************************************************************
  *   END: Testing with Real CSV. :END
 *********************************************************************/
 
-/********************************************************************
- * Getting class after giving a full constructor
-*********************************************************************/
-console.log('\nSTART FULL-CONSTRUCTOR TEST:');
-var constructed = csvr(target, {initAcc:[]}, function(acc, curr){acc.push(curr); return acc;});
-console.log(constructed.getObject());
-// console.log(constructed.reducedValue);
-console.log(constructed.getCSV());
-// console.log(constructed.newCSV);
+// /********************************************************************
+//  * Getting class after giving a full constructor
+// *********************************************************************/
+// console.log('\nSTART FULL-CONSTRUCTOR TEST:');
+// var constructed = csvr(target, {initAcc:[]}, function(acc, curr){acc.push(curr); return acc;});
+// console.log(constructed.getObject());
+// // console.log(constructed.reducedValue);
+// console.log(constructed.getCSV());
+// // console.log(constructed.newCSV);
 
-/********************************************************************
- * getting a class with an empty constructor
-*********************************************************************/
-console.log('\nSTART EMPTY-CONSTRUCTOR TEST:');
-var unconstructed = csvr();
-var targetNoBOM = unconstructed.removeBOM(target);
-console.log(targetNoBOM); console.log();
+// /********************************************************************
+//  * getting a class with an empty constructor
+// *********************************************************************/
+// console.log('\nSTART EMPTY-CONSTRUCTOR TEST:');
+// var unconstructed = csvr();
+// var targetNoBOM = unconstructed.removeBOM(target);
+// console.log(targetNoBOM); console.log();
 
 
 
