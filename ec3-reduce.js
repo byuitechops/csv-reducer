227f319423a3ebcf43df31306ffa90c77454be51
Aaron Shiffler <ajshiffler@gmail.com>
2018-07-20 11:44:41 -0600

    Started remove everything between code. Regex for it also found in parsing-conditions.txt

--- ec3-reduce.js
+++ ec3-reduce.js
@@ -1,22 +1,22 @@
 /********************************************************************
  * Declare Dependancies
-*********************************************************************/
-const fs =  require('fs');
+ *********************************************************************/
+const fs = require('fs');
 const csvr = require('./main.js');
 var fileNumber = parseInt(process.argv[2], 10);
-if (process.argv[2] === undefined){
+if (process.argv[2] === undefined) {
     fileNumber = 0;
 }
 
 /********************************************************************
  * Test Files
-*********************************************************************/
+ *********************************************************************/
 const dummyTarget = './csv-tests/countriesDummyData.csv';
 const bomRemoval = './csv-tests/bom.txt';
 
 /********************************************************************
  * ec3 Test Files
-*********************************************************************/
+ *********************************************************************/
 const targetDirectory = './csv-tests/ec3/';
 const targetFiles = [
     'FP_L1_DE_T11_POC4_V1_CSS.csv',
@@ -26,7 +26,7 @@ const targetFiles = [
 ];
 
 var updateCanDoField = false;
-if ( targetFiles[fileNumber].includes('R') || targetFiles[fileNumber].includes('W') ) {
+if (targetFiles[fileNumber].includes('R') || targetFiles[fileNumber].includes('W')) {
     updateCanDoField = true;
     console.log('This File Needs to have the can-do field updated.');
 }
@@ -47,48 +47,86 @@ var outputName = targetFile;
 var csv = fs.readFileSync(targetDirectory + targetFile, 'utf8');
 
 // Declare Options
-csvrOptions = {
-    headersOut:[
-        'id','skill','level','difficultylevel','function','passagetext',
-        'passagetexttype','passagetype','passageaudiotranscript','passagename','questionname','questioncando',
-        'questiontext','questionlevelfeedback','questiontype','questionaudiotranscript',
-        'answertext1','answertext2','answertext3','answertext4','answertext5','answertext6'
+var csvrOptions = {
+    headersOut: [
+        'id', 'skill', 'level', 'difficultylevel', 'function', 'passagetext',
+        'passagetexttype', 'passagetype', 'passageaudiotranscript', 'passagename', 'questionname', 'questioncando',
+        'questiontext', 'questionlevelfeedback', 'questiontype', 'questionaudiotranscript',
+        'answertext1', 'answertext2', 'answertext3', 'answertext4', 'answertext5', 'answertext6'
     ],
-    initAcc:[]
+    initAcc: []
 };
 
 // Main Reducer Function
-reducer = function(acc, curr) {
+var reducer = function (acc, curr) {
     //Cycle through all headers. Depending on the header, do this:
-    csvrOptions.headersOut.forEach((header) => {
-        if (header === 'questioncando' && updateCanDoField) {  // if questioncando field, 
-            if (curr.questioncando === 'f9'){                  // and file is for read or write,
-                console.log("Updating f9 to f10!");
+    Object.keys(curr).forEach((header) => {
+        if (header === 'questioncando' && updateCanDoField) { // if questioncando field, 
+            if (curr.questioncando === 'f9') { // and file is for read or write,
+                console.log('Updating f9 to f10!');
                 curr.questioncando = 'f10'; // f9 to f10
-            } else if (curr.questioncando === 'f10'){
-                console.log("Updating f10 to f11!");
+            } else if (curr.questioncando === 'f10') {
+                console.log('Updating f10 to f11!');
                 curr.questioncando = 'f11'; // f10 to f11
-            } else if (curr.questioncando === 'f11'){
-                console.log("Updating f11 to f9!");
+            } else if (curr.questioncando === 'f11') {
+                console.log('Updating f11 to f9!');
                 curr.questioncando = 'f9'; // f11 to f9
-            } else if (curr.questioncando === 'f31'){
-                console.log("Updating f31 to f30!");
+            } else if (curr.questioncando === 'f31') {
+                console.log('Updating f31 to f30!');
                 curr.questioncando = 'f30'; // f31 to f30
             }
-        } else if (header === 'questionname') {
+        } else if (header === 'questionname') { // if questionname field,
             var pqname = curr.questionname;
-            var qname = pqname.split(/passage\s*\d+\s*/)[1];
-            var pname = pqname.split(/question\s*\d+\s*/)[0];
-            curr.questionname = qname.replace(/\s/g, '');
-            curr.passagename = pname.replace(/\s/g, '');
-        } else if (header === 'passageaudiofilename') {
-            curr.passageaudiotranscript = curr.passageaudiofilename;
+            var qname = pqname.split(/passage\s*\d+\s*/)[1]; // isolate the question#
+            var pname = pqname.split(/question\s*\d+\s*/)[0]; // isolate the passage#
+            curr.questionname = qname.replace(/\s/g, ''); // save, removing spaces
+            curr.passagename = pname.replace(/\s/g, ''); // save, removing spaces
+        } else if (header === 'passageaudiofilename') { // if passageaudiofilename field,
+            curr.passageaudiotranscript = curr.passageaudiofilename; // change value to new header name
             delete curr.passageaudiofilename;
-        } else if (header === 'questionaudiofilename') {
-            curr.questionaudiotranscript = curr.questionaudiofilename;
+        } else if (header === 'questionaudiofilename') { // if passageaudiofilename field,
+            curr.questionaudiotranscript = curr.questionaudiofilename; // change value to new header name
             delete curr.questionaudiofilename;
-        } else if (header === 'passagetext') {
-            
+        } else if (header === 'passagetext') { // if passagetext field,
+            // instructions
+            var instructionsLocaiton = curr.passagetext.indexOf('<h1>Instructions</h1>');
+            var instructionsFound = instructionsLocaiton !== -1; // was the instructions header found?
+            // warmup
+            var warmUpLocation = curr.passagetext.indexOf('<h2>Warm-up</h2>');
+            var warmUpFound = warmUpLocation !== -1; // was the warm up location found?
+            // passage
+            var passageLocation = curr.passagetext.indexOf('<h2>Passage</h2>');
+            var passageFound = passageLocation !== -1; // was the passage location found?
+            // determine start Location
+            var startDelete;
+            var endDelete;
+            if (instructionsFound) {
+                startDelete = instructionsLocaiton;
+                if (warmUpFound && passageFound) {
+                    if (warmUpLocation < passageLocation) {
+                        endDelete = warmUpLocation;
+                        console.log('choosing warmup');
+                    } else {
+                        endDelete = passageLocation;
+                        console.log('choosing passage');
+                    }
+                } else if (warmUpFound && !passageFound) {
+                    console.log('choosing warmup');
+                    endDelete = warmUpLocation;
+                } else if (passageFound && !warmUpFound) {
+                    endDelete = passageLocation;
+                    console.log('choosing passage');
+                } else {
+                    throw 'In remove passage content, an unhandeled error occurred.';
+                }
+            } else {
+                console.error('The \'<h1>Instructions</h1>\' header wasn\'t found.');
+                console.error('Skipping Element: ' + acc.length());
+            }
+            console.log(startDelete);
+            console.log(endDelete);
+        } else if (header === true) {
+            console.log();
         }
     });
     acc.push(curr);
@@ -99,19 +137,14 @@ reducer = function(acc, curr) {
 var newcsv = csvr(csv, csvrOptions, reducer);
 var csvOutput = newcsv.getFormattedCSV();
 
-console.log(JSON.stringify(newcsv.getReducedCSV(), null, 4));
+// console.log(JSON.stringify(newcsv.getReducedCSV(), null, 4));
 
 // Output File
 var outputLocation = outputDirectory + Date.now() + '_' + outputName;
-fs.writeFile(outputLocation, csvOutput, function(err){
+fs.writeFile(outputLocation, csvOutput, function (err) {
     if (err) {
         console.error(err);
     } else {
         console.log('Output file to: ' + outputLocation);
     }
 });
-
-
-
-
-
--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 /********************************************************************
  * Require Dependancies Here
-*********************************************************************/
+ *********************************************************************/
 const dsv = require('d3-dsv');
 
 /********************************************************************
  * This will check for and remove the Byte-Order-Mark if it exists
  * in the given CSV document Note: BOM can be referenced by:
  * '\ufeff' in utf8, and shows up as 'ef bb bf' in buffer form
-*********************************************************************/
-const removeBOM = function(stringWithBOM){
+ *********************************************************************/
+const removeBOM = function (stringWithBOM) {
     console.log('removeBOM()');
     try {
-        return stringWithBOM.replace(/\ufeff/g,'');
+        return stringWithBOM.replace(/\ufeff/g, '');
     } catch (error) {
         console.error('OUTPUT FAILED. THE INPUT NEEDS TO BE A STRING:');
         console.error(error);
@@ -20,8 +20,8 @@ const removeBOM = function(stringWithBOM){
 
 /********************************************************************
  * This will use d3-dsv to turn the csv into a json object.
-*********************************************************************/
-const parseCSV = function(csvToParse){
+ *********************************************************************/
+const parseCSV = function (csvToParse) {
     console.log('parseCSV()');
     return dsv.csvParse(csvToParse);
 }
@@ -29,8 +29,8 @@ const parseCSV = function(csvToParse){
 /********************************************************************
  * csvReducer takes the parsed csv and edits it based on the given
  * function and the options specified
-*********************************************************************/
-const csvReducer = function(csvToReduce, options, reducerFunction){
+ *********************************************************************/
+const csvReducer = function (csvToReduce, options, reducerFunction) {
     console.log('csvReducer()');
     var initAcc = Object.assign([], options.initAcc); // How to copy an object 
     var reducedCSV = csvToReduce.reduce(reducerFunction, initAcc);
@@ -42,28 +42,26 @@ const csvReducer = function(csvToReduce, options, reducerFunction){
  * Edits the current CSV Object and removes any keys that don't match the
  * headersOut option.
  *********************************************************************/
-const limitHeaders = function(csvObject, options){
+const limitHeaders = function (csvObject, options) {
     // Choose what to do here based on options
     console.log('limitHeaders()');
-    reducedCSV = csvObject.reduce(function(acc, curr){
-        csvObject.columns.forEach(function(column){
+    reducedCSV = csvObject.reduce(function (acc, curr) {
+        csvObject.columns.forEach(function (column) {
             var doKeepKey = [];
-            options.headersOut.forEach(function(header) {
+            options.headersOut.forEach(function (header) {
                 if (column === header) {
                     doKeepKey.push(true);
-                } else {
-
                 }
             });
-            if (doKeepKey.includes(true)){
-                
+            if (doKeepKey.includes(true)) {
+
             } else {
                 delete curr[column];
             }
         });
         acc.push(curr);
         return acc;
-    },[]);
+    }, []);
     // console.log(reducedCSV)
     return reducedCSV;
 }
@@ -71,8 +69,8 @@ const limitHeaders = function(csvObject, options){
 /********************************************************************
  * formatCSV() takes the current CSV Object and formats it into a 
  * CSV String.
-*********************************************************************/
-const formatCSV = function(csvObject, options){
+ *********************************************************************/
+const formatCSV = function (csvObject, options) {
     console.log('formatCSV()');
     return dsv.csvFormat(csvObject, options.headersOut);
 }
@@ -81,7 +79,7 @@ const formatCSV = function(csvObject, options){
 /********************************************************************
  * 
  *********************************************************************/
-const optionsChecker = function(options){
+const optionsChecker = function (options) {
     // Choose what to do here based on options
     console.log('optionsChecker()');
     // go through each option and set default values to anything not filled out 
@@ -91,9 +89,9 @@ const optionsChecker = function(options){
 
 /********************************************************************
  * 
-*********************************************************************/
+ *********************************************************************/
 class ModifiedCSV {
-    constructor(initialCSV, options, reducerFunction){
+    constructor(initialCSV, options, reducerFunction) {
         if (initialCSV !== undefined) {
             this.options = this.optionsChecker(options);
             this.initialCSV = this.removeBOM(initialCSV); // initial copy of the given csv
@@ -105,24 +103,42 @@ class ModifiedCSV {
         }
     }
     // methods
-    optionsChecker(options){return optionsChecker(options);}
-    removeBOM(stringWithBOM){return removeBOM(stringWithBOM);}
-    parseCSV(csvToParse){return parseCSV(csvToParse);}
-    csvReducer(csvToReduce, reducerFunction){return csvReducer(csvToReduce, this.options, reducerFunction);}
-    limitHeaders(parsedCSV){return limitHeaders(parsedCSV, this.options);}
-    formatCSV(csvToFormat){return formatCSV(csvToFormat, this.options);}
+    optionsChecker(options) {
+        return optionsChecker(options);
+    }
+    removeBOM(stringWithBOM) {
+        return removeBOM(stringWithBOM);
+    }
+    parseCSV(csvToParse) {
+        return parseCSV(csvToParse);
+    }
+    csvReducer(csvToReduce, reducerFunction) {
+        return csvReducer(csvToReduce, this.options, reducerFunction);
+    }
+    limitHeaders(parsedCSV) {
+        return limitHeaders(parsedCSV, this.options);
+    }
+    formatCSV(csvToFormat) {
+        return formatCSV(csvToFormat, this.options);
+    }
     // setters
-    setOptions(options){return optionsChecker(options);}
+    setOptions(options) {
+        return optionsChecker(options);
+    }
     // setInitialCSV(initialCSV){this.initialCSV = removeBOM(initialCSV);}
     // setParsedCSV(parsedCSV){this.parsedCSV = parsedCSV}
     // getters
-    getFormattedCSV(){return this.newCSV;}
-    getReducedCSV(){return this.reducedCSV;}
+    getFormattedCSV() {
+        return this.newCSV;
+    }
+    getReducedCSV() {
+        return this.reducedCSV;
+    }
 }
 
 /********************************************************************
  * a new class (or object) created based on the given parameters would be the export
-*********************************************************************/
+ *********************************************************************/
 module.exports = (initialCSV, options, reducerFunction) => {
     return new ModifiedCSV(initialCSV, options, reducerFunction);
 }
